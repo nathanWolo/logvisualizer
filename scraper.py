9f16db1f5eff43367ee87fd4f7870b5959a0b6c0
Nathan Woloshyn <nathanwoloshyn@gmail.com>
2021-08-21 20:50:53 -0700

    adding scraper

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-
+from player import Player
 base_url = "logs.tf/"
 cur_log = 1
-player_dict = {}
+player_list = []
 while cur_log < 100:
     cur_url = base_url + cur_log
     page = requests.get(cur_url)
-
+    
 
