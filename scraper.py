ded30f52c6cd6bfb7343e4d1ddf1936f0f3f9f65
Nathan Woloshyn <nathanwoloshyn@gmail.com>
2021-08-22 19:38:50 -0700

    updated scraper, scrapes first 1000 logs and prints sorted list of steamids by games played

--- scraper.py
+++ scraper.py
@@ -1,11 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 from player import Player
-base_url = "logs.tf/"
+import re
+base_url = "http://logs.tf/"
 cur_log = 1
+player_dict = {}
 player_list = []
-while cur_log < 100:
-    cur_url = base_url + cur_log
+while cur_log < 1000:
+    cur_url = base_url + str(cur_log)
     page = requests.get(cur_url)
-    
+    soup = BeautifulSoup(page.content, "html.parser")
+    players = soup.find_all("tr", id = re.compile("player_\d{17}"))
+    steam_idre = re.search("player_\d{17}", str(players))
+    if steam_idre:
+        steam_id = steam_idre.group(0)
+        steam_id = steam_id[7:]
+        if steam_id in player_dict.keys():
+            player_dict[steam_id] += 1
+        else:
+            player_dict[steam_id] = 1
+            player_list.append(steam_id)
+    cur_log += 1
+    print(cur_log)
+sorted_list = sorted(player_list, key = lambda k: player_dict[k], reverse=True)
+for p in sorted_list:
+    print("SteamID:" + p + "  Games Played:" + str(player_dict[p]))
 
