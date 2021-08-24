e09d594e4cfe435497bef64bbd8b183f9c79b8f1
Nathan Woloshyn <nathanwoloshyn@gmail.com>
2021-08-23 18:01:01 -0700

    Fixed bug where only one player per log was tallied

--- scraper.py
+++ scraper.py
@@ -6,20 +6,27 @@ base_url = "http://logs.tf/"
 cur_log = 1
 player_dict = {}
 player_list = []
-while cur_log < 1000:
+while cur_log < 100:
     cur_url = base_url + str(cur_log)
     page = requests.get(cur_url)
     soup = BeautifulSoup(page.content, "html.parser")
     players = soup.find_all("tr", id = re.compile("player_\d{17}"))
-    steam_idre = re.search("player_\d{17}", str(players))
-    if steam_idre:
-        steam_id = steam_idre.group(0)
-        steam_id = steam_id[7:]
-        if steam_id in player_dict.keys():
-            player_dict[steam_id] += 1
+    steam_idre = re.findall("player_\d{17}", str(players))
+    for match in steam_idre:
+        #print(match)
+        if match[7:] in player_dict.keys():
+            player_dict[match[7:]] += 1
         else:
-            player_dict[steam_id] = 1
-            player_list.append(steam_id)
+            player_dict[match[7:]] = 1
+            player_list.append(match[7:])
+    # if steam_idre:
+    #     steam_id = steam_idre.group(0)
+    #     steam_id = steam_id[7:]
+    #     if steam_id in player_dict.keys():
+    #         player_dict[steam_id] += 1
+    #     else:
+    #         player_dict[steam_id] = 1
+    #         player_list.append(steam_id)
     cur_log += 1
     print(cur_log)
 sorted_list = sorted(player_list, key = lambda k: player_dict[k], reverse=True)
